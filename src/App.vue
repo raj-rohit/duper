cf483e7b3f9e6d53822be91aaea5cf8bb00777f7
rohit-een <58426907+rohit-een@users.noreply.github.com>
2020-04-22 21:27:25 +0530

    Added submodule

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Hero></Hero>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Hero from '../more/src/components/Hero'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+     Hero
   }
 }
 </script>
